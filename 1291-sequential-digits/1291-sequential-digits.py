e0baf333cff032013263a434d402547252536d99
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-07-14 00:17:14 +0530

    Time: 0 ms (100%), Space: 12.6 MB (0%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,5 +1,10 @@
-class Solution:
-    def sequentialDigits(self, low: int, high: int) -> List[int]:
+class Solution(object):
+    def sequentialDigits(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: List[int]
+        """
         ans = []
         s = "123456789"
         for length in range(2, 10):
@@ -7,5 +12,6 @@ class Solution:
                 num = int(s[start:start + length])
                 if low <= num <= high:
                     ans.append(num)
+        ans.sort()
         return ans
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
