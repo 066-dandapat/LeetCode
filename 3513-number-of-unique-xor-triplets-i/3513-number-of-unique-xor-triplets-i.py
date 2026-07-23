4a172cb622a42038889ee95549369832d6cce549
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-07-23 21:10:56 +0530

    Time: 0 ms (100%), Space: 34.1 MB (27.12%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def uniqueXorTriplets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
         if n < 3:
             return n
-        return 1 << (n.bit_length())
+        return 1 << n.bit_length()
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
