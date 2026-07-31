0cc038f0864ad5dd604ce1b98de2688ee605933b
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-07-31 17:18:01 +0530

    Time: 0 ms (100%), Space: 20.1 MB (48.16%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,15 +1,11 @@
-class Solution(object):
-    def minimumPushes(self, word):
-        """
-        :type word: str
-        :rtype: int
-        """
+class Solution:
+    def minimumPushes(self, word: str) -> int:
         freq = {}
         for ch in word:
             freq[ch] = freq.get(ch, 0) + 1
         counts = sorted(freq.values(), reverse=True)
         ans = 0
-        for i in range(len(counts)):
-            ans += counts[i] * (i // 8 + 1)
+        for i, f in enumerate(counts):
+            ans += f * (i // 8 + 1)
         return ans
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
