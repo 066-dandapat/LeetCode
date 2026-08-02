73ec397972cf641b22d5761740d3d3cf7ed7f925
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-02 18:25:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.87%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def stoneGame(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
         return True
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
