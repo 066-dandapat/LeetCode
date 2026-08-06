2b73947ba24a5822da49d39629f0159a283fafda
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-06 20:09:28 +0530

    Time: 0 ms (100%), Space: 12.3 MB (62.03%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,12 +1,16 @@
-class Solution:
-    def smallestNumber(self, n: int, t: int) -> int:
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
         while True:
             x = n
             prod = 1
             while x > 0:
                 prod *= x % 10
                 x //= 10
-
             if prod % t == 0:
                 return n
             n += 1
