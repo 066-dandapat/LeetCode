6cfa4cd61b5c883f8d935800cfc7cdc14d33f6c2
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-08 22:13:40 +0530

    Time: 0 ms (100%), Space: 46.7 MB (86.96%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def validSequence(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: List[int]
-        """
+class Solution:
+    def validSequence(self, word1: str, word2: str) -> List[int]:
         n = len(word1)
         m = len(word2)
         suf = [-1] * (m + 1)
@@ -14,23 +9,19 @@ class Solution(object):
             if j >= 0 and word1[i] == word2[j]:
                 suf[j] = i
                 j -= 1
-        if j >= 0:
-            pass
         ans = []
         j = 0
-        used = False
+        changed = False
         for i in range(n):
             if j == m:
                 break
             if word1[i] == word2[j]:
                 ans.append(i)
                 j += 1
-            elif not used:
-                if j + 1 == m or suf[j + 1] > i:
+            elif not changed:
+                if j + 1 == m or (suf[j + 1] != -1 and suf[j + 1] > i):
                     ans.append(i)
                     j += 1
-                    used = True
-        if j == m:
-            return ans
-        return []
+                    changed = True
+        return ans if j == m else []
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
