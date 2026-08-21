c0077ee02bac9a3f79afe16eab30d37d3a19757d
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-21 17:19:04 +0530

    Time: 1758 ms (6.1%), Space: 19.4 MB (78.05%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -1,41 +1,44 @@
-class Solution(object):
-    def findKthSmallest(self, coins, k):
-        """
-        :type coins: List[int]
-        :type k: int
-        :rtype: int
-        """
-        def gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
+class Solution:
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        from math import gcd
+
         def lcm(a, b):
             return a // gcd(a, b) * b
+
         def count(x):
             n = len(coins)
             total = 0
+
+            # Inclusion-exclusion over all subsets
             for mask in range(1, 1 << n):
                 val = 1
                 bits = 0
+
                 for i in range(n):
                     if mask & (1 << i):
                         val = lcm(val, coins[i])
                         if val > x:
                             break
                         bits += 1
+
                 if val <= x:
-                    if bits % 2 == 1:
-                        total += x // val
+                    cur = x // val
+                    if bits % 2:
+                        total += cur
                     else:
-                        total -= x // val
+                        total -= cur
+
             return total
+
         left = 1
         right = min(coins) * k
+
         while left < right:
             mid = (left + right) // 2
+
             if count(mid) >= k:
                 right = mid
             else:
                 left = mid + 1
+
         return left
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
