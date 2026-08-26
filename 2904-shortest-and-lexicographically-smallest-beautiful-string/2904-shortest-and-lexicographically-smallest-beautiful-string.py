23fb5e69d9679e7d7b5e3f18a5e697ec238a6d2b
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-26 15:11:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (86.96%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,43 +1,20 @@
-class Solution:
-    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        start = 0
-        end = 0
-        count = 0
-        smallest = [0, 0, 0]
-        for ch in s:
-            end += 1
-            if ch == '1':
-                count += 1
-            while count >= k and start < end:
-                if smallest[0] == 0 or (end - start) < smallest[0]:
-                    smallest[0] = end - start
-                    smallest[1] = start
-                    smallest[2] = end
-                elif (end - start) == smallest[0]:
-                    previous_substring = s[
-                        smallest[1]:smallest[2]
-                    ]
-                    current_substring = s[start:end]
-                    if self.is_current_smaller(
-                        previous_substring,
-                        current_substring
-                    ):
-                        smallest[0] = end - start
-                        smallest[1] = start
-                        smallest[2] = end
-                if s[start] == '1':
-                    count -= 1
-                start += 1
-        return s[smallest[1]:smallest[2]]
-    def is_current_smaller(
-        self,
-        previous_substring: str,
-        current_substring: str
-    ) -> bool:
-        for i in range(len(current_substring)):
-            if previous_substring[i] < current_substring[i]:
-                return False
-            elif previous_substring[i] > current_substring[i]:
-                return True
-        return True
+class Solution(object):
+    def shortestBeautifulSubstring(self, s, k):
+        left = 0
+        ones = 0
+        ans = ""
+        for right in range(len(s)):
+            if s[right] == '1':
+                ones += 1
+            while ones == k:
+                while left <= right and s[left] == '0':
+                    left += 1
+                curr = s[left:right + 1]
+                if (ans == "" or
+                    len(curr) < len(ans) or
+                    (len(curr) == len(ans) and curr < ans)):
+                    ans = curr
+                ones -= 1
+                left += 1
+        return ans
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
