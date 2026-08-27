9c592715ddabf5469976fa60fd3545bdf7071a2c
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-08-27 15:31:22 +0530

    Time: 3 ms (97.37%), Space: 19.3 MB (71.05%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,35 +1,45 @@
-class Solution(object):
-    def lexGreaterPermutation(self, s, target):
+class Solution:
+    def lexGreaterPermutation(self, s: str, target: str) -> str:
         cnt = [0] * 26
+
         for ch in s:
             cnt[ord(ch) - ord('a')] += 1
+
         ans = []
+
+        # Try to match target from left to right.
         for i in range(len(target)):
-            x = ord(target[i]) - ord('a')
+            x = ord(target[i]) - ord("a")
+
             if cnt[x] > 0:
                 cnt[x] -= 1
                 ans.append(target[i])
             else:
-                found = False
+                # Use the smallest character greater than target[i].
                 for c in range(x + 1, 26):
                     if cnt[c] > 0:
-                        ans.append(chr(c + ord('a')))
+                        ans.append(chr(c + ord("a")))
                         cnt[c] -= 1
+
                         for j in range(26):
-                            ans += [chr(j + ord('a'))] * cnt[j]
-                        return ''.join(ans)
-                found = False
+                            ans.extend([chr(j + ord("a"))] * cnt[j])
+
+                        return "".join(ans)
                 break
+
+        # Backtrack to make an earlier position larger.
         for i in range(len(ans) - 1, -1, -1):
-            old = ord(ans[i]) - ord('a')
+            old = ord(ans[i]) - ord("a")
             cnt[old] += 1
+
             for c in range(old + 1, 26):
                 if cnt[c] > 0:
-                    result = ans[:i]
-                    result.append(chr(c + ord('a')))
+                    result = ans[:i] + [chr(c + ord("a"))]
                     cnt[c] -= 1
+
                     for j in range(26):
-                        result += [chr(j + ord('a'))] * cnt[j]
-                    return ''.join(result)
+                        result.extend([chr(j + ord("a"))] * cnt[j])
+
+                    return "".join(result)
+
         return ""
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
