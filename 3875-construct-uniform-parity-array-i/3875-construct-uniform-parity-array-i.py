2bd0e7b88d37fa1762697d497a3914c05a47ad8b
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-02 15:01:01 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.38%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def uniformArray(self, nums1):
-        """
-        :type nums1: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
         return True
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
