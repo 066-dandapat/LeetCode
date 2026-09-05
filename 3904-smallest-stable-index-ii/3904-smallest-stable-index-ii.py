44c1061ac0f330c7230bd90bfdaf6d1993534bfb
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-05 17:57:26 +0530

    Time: 0 ms (100%), Space: 22.1 MB (51.61%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,8 +1,13 @@
-class Solution:
-    def firstStableIndex(self, nums: list[int], k: int) -> int:
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
         n = len(nums)
         suffix_min = [0] * n
-        suffix_min[-1] = nums[-1]
+        suffix_min[n - 1] = nums[n - 1]
         for i in range(n - 2, -1, -1):
             suffix_min[i] = min(nums[i], suffix_min[i + 1])
         prefix_max = nums[0]
