88e20ceeebf66ac7a586350721bf9a4141986f83
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-15 20:16:22 +0530

    Time: 0 ms (100%), Space: 50.6 MB (30.41%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,28 +1,19 @@
-class Solution(object):
-    def maxPalindromes(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def maxPalindromes(self, s: str, k: int) -> int:
         n = len(s)
-        count = 0
-        i = 0
-        def isPalindrome(left, right):
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
-        while i + k <= n:
-            if isPalindrome(i, i + k - 1):
-                count += 1
-                i += k
-            elif i + k < n and isPalindrome(i, i + k):
-                count += 1
-                i += k + 1
-            else:
-                i += 1
-        return count
+        pal = [[False] * n for _ in range(n)]
+        for i in range(n):
+            pal[i][i] = True
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and (length == 2 or pal[i + 1][j - 1]):
+                    pal[i][j] = True
+        dp = [0] * (n + 1)
+        for i in range(n):
+            dp[i + 1] = max(dp[i + 1], dp[i])
+            for j in range(i + k - 1, n):
+                if pal[i][j]:
+                    dp[j + 1] = max(dp[j + 1], dp[i] + 1)
+        return dp[n]
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
