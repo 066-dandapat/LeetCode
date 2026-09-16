f02b723af7ea2d99cc9bc9b740aa0ec92e64d77a
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-16 10:54:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.21%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,8 +1,9 @@
-class Solution(object):
-    def numberOfSets(self, n, k):
-        MOD = 1000000007
+class Solution:
+    def numberOfSets(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
         ans = 1
         for i in range(1, 2 * k + 1):
             ans = ans * (n + k - i) // i
+        
         return ans % MOD
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
