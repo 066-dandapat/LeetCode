c6500ce743a2a00ada6eaf726807df02b235441d
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-16 10:54:27 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.21%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -4,6 +4,5 @@ class Solution:
         ans = 1
         for i in range(1, 2 * k + 1):
             ans = ans * (n + k - i) // i
-        
         return ans % MOD
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
