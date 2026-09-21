1ed46f8d5abaf22b61740e1e1e62005b4dba7701
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-21 20:36:29 +0530

    Time: 0 ms (100%), Space: 34.1 MB (85.53%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -1,11 +1,5 @@
-class Solution(object):
-
-    def resultArray(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
         ans = [0] * k
         prev = [0] * k
         for x in nums:
