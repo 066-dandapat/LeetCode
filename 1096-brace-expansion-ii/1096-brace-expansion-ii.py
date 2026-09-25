020a1c94a8eed5d841ca072edd351ed9c31061af
sUBHAM DANDAPAT <dandapatsubham066@gmail.com>
2026-09-25 20:54:41 +0530

    Time: 0 ms (100%), Space: 12.7 MB (26.67%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.py
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.py
@@ -1,29 +1,26 @@
-class Solution:
-    def braceExpansionII(self, expression: str) -> list[str]:
-        def parse(i):
+class Solution(object):
+    def braceExpansionII(self, expression):
+        """
+        :type expression: str
+        :rtype: List[str]
+        """
+        def parse(s, i):
             result = set()
             current = {""}
-            while i < len(expression) and expression[i] != '}':
-                if expression[i] == '{':
-                    sub, i = parse(i + 1)
-                    current = {
-                        a + b
-                        for a in current
-                        for b in sub
-                    }
-                elif expression[i] == ',':
+            while i < len(s) and s[i] != '}':
+                if s[i] == '{':
+                    sub, i = parse(s, i + 1)
+                    current = {a + b for a in current for b in sub}
+                elif s[i] == ',':
                     result.update(current)
                     current = {""}
                     i += 1
                 else:
-                    ch = expression[i]
-                    current = {
-                        s + ch
-                        for s in current
-                    }
+                    ch = s[i]
+                    current = {x + ch for x in current}
                     i += 1
             result.update(current)
             return result, i + 1
-        result, _ = parse(0)
+        result, _ = parse(expression, 0)
         return sorted(result)
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
